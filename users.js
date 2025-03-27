dc64e13dc759109c1920d2f00b37c214635346d4
nianod <arnoldkk422@gmail.com>
2025-03-27 10:30:23 +0300

    first commit

---
+++ users.js
@@ -0,0 +1,20 @@
+
+
+
+import { asyncHandler } from "../utils.asyncHandler.js" //Error handling
+
+const registerUser = asyncHandler (async (req, res) => {
+    // Get user details from Fronted
+    // validation (should not be null)
+    // Check if users' name and email exists
+    // check for images and avatar
+    // Upload them in cloudinary
+    // Create user object,,,,db entry
+    // Remove password and refresh token field
+    // Check user creation
+    // Return response
+
+
+});
+
+export { registerUser }
