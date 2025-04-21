e25dfb3ab21f45444cbc9b5592b30b8605801f2e
nianod <arnoldkk422@gmail.com>
2025-04-21 17:06:31 +0300

    initialized chat

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,10 @@
 const readline = require("readline")
 const replies = {
-    "hello": "Yes yooh hows you"
+    "hello": "Yes yooh hows you",
+    "how are you?": "Am good, whatsup",
+    "Who are you": "Am an AI model(Chatbot)", 
+    "default": "I did not undertand that"
 }
+const rl = readline.createInterface({
+    input:process.stdin
+})
