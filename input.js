30ec4f3e224e96c51bbad330710e560ff0301a5b
nianod <arnoldkk422@gmail.com>
2025-04-24 17:07:12 +0300

    finales

---
+++ input.js
@@ -0,0 +1,21 @@
+
+import UserList from "./store.txt";
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+rl.question("Enter your name: ", (event) => {
+    if(event === "") {
+        console.log("Cannot be empty");
+        
+    } else {
+        next()
+    }
+    rl.close();
+})
+
+const next = () => {
+    let validation
+}
