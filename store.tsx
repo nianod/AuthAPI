19509d8928a918d56460a589613321e2807b327e
nianod <arnoldkk422@gmail.com>
2025-05-18 23:33:26 +0300

    fragment

--- store.tsx
+++ store.tsx
@@ -1,29 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from './userSlice';
-import { RootState } from './store';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
+import { RootState } from "./store";
 
 const UserList: React.FC = () => {
   const dispatch = useDispatch();
-  const { users, status, error } = useSelector((state: RootState) => state.users);
+  const { users, status, error } = useSelector(
+    (state: RootState) => state.users
+  );
 
   useEffect(() => {
-    if (status === 'idle') {
+    if (status === "idle") {
       dispatch(fetchUsers());
     }
   }, [status, dispatch]);
 
   return (
-    <div>
-      <h1>Users</h1>
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'failed' && <p>Error: {error}</p>}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Users</h1>
+        {status === "loading" && <p>Loading...</p>}
+        {status === "failed" && <p>Error: {error}</p>}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
